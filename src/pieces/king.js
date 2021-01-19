1da46db19dbcd1ec1e970525857b811e4cc01c96
Terry <t3rrygong@gmail.com>
2021-01-19 14:25:45 -0700

    king's possible moves function added

--- src/pieces/king.js
+++ src/pieces/king.js
@@ -19,7 +19,86 @@ export default class King extends Piece {
 
     possibleMoves(src, squares) {
         const highLightMoves = [];
-
+        //go left
+        if (squares[src - 1] === null && src % 8 !== 0) {
+            highLightMoves.push((src - 1));
+        }
+        if (squares[src - 1] !== null && src % 8 !== 0 && squares[src - 1].player === 1 && squares[src].player === 2) {
+            highLightMoves.push((src - 1));
+        }
+        if (squares[src - 1] !== null && src % 8 !== 0 && squares[src - 1].player === 2 && squares[src].player === 1) {
+            highLightMoves.push((src - 1));
+        }
+        //go right
+        if (squares[src + 1] === null && (src + 1) % 8 !== 0) {
+            highLightMoves.push((src + 1));
+        }
+        if (squares[src + 1] !== null && (src + 1) % 8 !== 0 && squares[src + 1].player === 1 && squares[src].player === 2) {
+            highLightMoves.push((src + 1));
+        }
+        if (squares[src + 1] !== null && (src + 1) % 8 !== 0 && squares[src + 1].player === 2 && squares[src].player === 1) {
+            highLightMoves.push((src + 1));
+        }
+        //go up
+        if (squares[src - 8] === null && (src - 8) > -1) {
+            highLightMoves.push((src - 8));
+        }
+        if (squares[src - 8] !== null && (src - 8) > -1 && squares[src - 8].player === 1 && squares[src].player === 2) {
+            highLightMoves.push((src - 8));
+        }
+        if (squares[src - 8] !== null && (src - 8) > -1 && squares[src - 8].player === 2 && squares[src].player === 1) {
+            highLightMoves.push((src - 8));
+        }
+        //go down
+        if (squares[src + 8] === null && (src + 8) < 64) {
+            highLightMoves.push((src + 8));
+        }
+        if (squares[src + 8] !== null && (src + 8) < 64 && squares[src + 8].player === 1 && squares[src].player === 2) {
+            highLightMoves.push((src + 8));
+        }
+        if (squares[src + 8] !== null && (src + 8) < 64 && squares[src + 8].player === 2 && squares[src].player === 1) {
+            highLightMoves.push((src + 8));
+        }
+        //go upleft
+        if ((src - 7) > -1 && squares[src - 7] === null && src % 8 !== 0) {
+            highLightMoves.push((src - 7));
+        }
+        if ((src - 7) > -1 && squares[src - 7] !== null && src % 8 !== 0 && squares[src - 7].player === 1 && squares[src].player === 2) {
+            highLightMoves.push((src - 7));
+        }
+        if ((src - 7) > -1 && squares[src - 7] !== null && src % 8 !== 0 && squares[src - 7].player === 2 && squares[src].player === 1) {
+            highLightMoves.push((src - 7));
+        }
+        //go upright
+        if ((src - 9) > -1 && squares[src - 9] === null && (src + 1) % 8 !== 0) {
+            highLightMoves.push((src - 9));
+        }
+        if ((src - 9) > -1 && squares[src - 9] !== null && (src + 1) % 8 !== 0 && squares[src - 9].player === 1 && squares[src].player === 2) {
+            highLightMoves.push((src - 9));
+        }
+        if ((src - 9) > -1 && squares[src - 9] !== null && (src + 1) % 8 !== 0 && squares[src - 9].player === 2 && squares[src].player === 1) {
+            highLightMoves.push((src - 9));
+        }
+        //go downleft
+        if ((src + 7) < 64 && squares[src + 7] === null && src % 8 !== 0) {
+            highLightMoves.push((src + 7));
+        }
+        if ((src + 7) < 64 && squares[src + 7] !== null && src % 8 !== 0 && squares[src + 7].player === 1 && squares[src].player === 2) {
+            highLightMoves.push((src + 7));
+        }
+        if ((src + 7) < 64 && squares[src + 7] !== null && src % 8 !== 0 && squares[src + 7].player === 2 && squares[src].player === 1) {
+            highLightMoves.push((src + 7));
+        }
+        //go downright
+        if ((src + 9) < 64 && squares[src + 9] === null && (src + 1) % 8 !== 0) {
+            highLightMoves.push((src + 9));
+        }
+        if ((src + 9) < 64 && squares[src + 9] !== null && (src + 1) % 8 !== 0 && squares[src + 9].player === 1 && squares[src].player === 2) {
+            highLightMoves.push((src + 9));
+        }
+        if ((src + 9) < 64 && squares[src + 9] !== null && (src + 1) % 8 !== 0 && squares[src + 9].player === 2 && squares[src].player === 1) {
+            highLightMoves.push((src + 9));
+        }
         return highLightMoves;
     }
 
