fdc266e38dd8648ba42eef47ae493349ec991834
Terry <t3rrygong@gmail.com>
2021-01-19 14:06:39 -0700

    added possiblemoves for queen

--- src/pieces/king.js
+++ src/pieces/king.js
@@ -3,6 +3,7 @@ import Piece from './piece.js';
 export default class King extends Piece {
     constructor(player) {
         super(player, (player === 1 ? "https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg" : "https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg"));
+        this.name = "King";
     }
 
     isMovePossible(src, dest) {
@@ -16,10 +17,12 @@ export default class King extends Piece {
             src - 1 === dest);
     }
 
-    /**
-     * always returns empty array because of one step
-     * @return {[]}
-     */
+    possibleMoves(src, squares) {
+        const highLightMoves = [];
+
+        return highLightMoves;
+    }
+
     getSrcToDestPath(src, dest) {
         return [];
     }
